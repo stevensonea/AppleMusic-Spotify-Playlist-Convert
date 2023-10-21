d81415f8ca0af133b099c5074fc6e811be5918a5
stevenea <stevenea@iu.edu>
2023-10-20 22:59:40 -0400

    device and account infromation found

---
+++ testRunCode/playlistMaker-Spotify.py
@@ -0,0 +1,13 @@
+""""
+Creator: Ethan Stevenson 
+Title: Playlist Maker for Spotify
+Purpose: Learning to work with Spoitpy library and creating playlist for my Spotify account so that I can utilize this information to make any Apple Music Playlist and Spotify Playlist and vice versa
+"""
+
+import spotipy as sp
+from spotipy.oauth2 import SpotifyOAuth
+
+#Inforamtion for my Spotify account and windows machine
+DEVICE_ID = "d7390bb1fd59b6ee0811700a4ee5be4c53b0e032"
+CLIENT_ID = "aacef9dd894042cc88b52aba5aa1e9d6"
+CLIENT_SECRET = "515d356ac5e6465b857628c155201bff"
