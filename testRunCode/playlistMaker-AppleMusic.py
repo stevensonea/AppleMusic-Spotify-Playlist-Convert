235c705d292b85a5ab5eb66dcd268f42f0244c22
stevenea <stevenea@iu.edu>
2023-10-21 23:39:11 -0400

    beginning to work with Apple Music

---
+++ testRunCode/playlistMaker-AppleMusic.py
@@ -0,0 +1,5 @@
+""""
+Creator: Ethan Stevenson 
+Title: Playlist Maker for Apple Music
+Purpose: Learning to work with Spoitpy library and creating playlist for my Spotify account so that I can utilize this information to make any Apple Music Playlist and Spotify Playlist and vice versa
+"""
